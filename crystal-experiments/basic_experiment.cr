0f7815a2b77355b1a3adae498e5260bc386ba727
Paul Nikitochkin <pftg@users.noreply.github.com>
2019-02-26 22:20:32 +0200

    Use local server for crystal

--- crystal-experiments/basic_experiment.cr
+++ crystal-experiments/basic_experiment.cr
@@ -1,22 +1,28 @@
 require "./test_data"
 require "http/client"
 
-ch = Channel(Nil).new
+start = Time.now
+
+ch = Channel(Nil).new(100)
 schedule_ch = Channel(Nil).new
 
-SHUFFLED_LINKS.each_slice(3) do |links|
+SHUFFLED_LINKS.each_slice(5) do |links|
   spawn do
-    schedule_ch.receive
+#    schedule_ch.receive
+
     links.each do |link|
       response = HTTP::Client.get link
-      puts response.status_code      # => 200
-      schedule_ch.send(nil)
-      ch.send(nil)
+#      puts response.status_code      # => 200
     end
+
+#    schedule_ch.send(nil)
+    ch.send(nil)
   end
 end
 
-25.times { schedule_ch.send(nil) }
-(SHUFFLED_LINKS.size / 3).times { ch.receive }
+#140.times { schedule_ch.send(nil) }
+(SHUFFLED_LINKS.size / 5).times { ch.receive }
 
 
+puts "#{Time.now - start} seconds"
+puts "Links: #{SHUFFLED_LINKS.size}"
